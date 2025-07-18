657696a627eeadb55660216032454be944550fd3
adolf <dlester626@gmail.com>
2025-07-18 20:55:09 +0100

    Update Fri 18 Jul 20:55:09 BST 2025

--- git_upload.sh
+++ git_upload.sh
@@ -3,20 +3,16 @@ set -e
 
 cd /home/adolf/neobench
 
-# Setup Git if not already initialized
 if [ ! -d .git ]; then
     git init
-    git config user.name "adolf"
-    git config user.email "adolf@neobench.org"
     git branch -M main
     git remote add origin https://github.com/NeoBench/NeoBench.git
-else
-    git remote set-url origin https://github.com/NeoBench/NeoBench.git
-    git branch -M main
+    git config user.name "ArchLinux Dev"
+    git config user.email "your@email.com"
 fi
 
 git add -A
-git commit -am "Update $(date)" || echo "Nothing to commit."
+git commit -m "Update $(date)" || echo "Nothing to commit."
 git push -u --force origin main
 
 echo "NeoBench GitHub sync complete: https://github.com/NeoBench/NeoBench"
